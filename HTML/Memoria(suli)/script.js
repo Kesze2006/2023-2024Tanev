a607447ef3974c6c808208681a6a359a3a96dbc5
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2024-01-29 12:43:48 +0100

    asd

--- HTML/Memoria(suli)/script.js
+++ HTML/Memoria(suli)/script.js
@@ -9,6 +9,9 @@ var kepekUrl=[
 "oliver.png",
 "penge.png"]
 
+var parDb = 4;
+var kattintas = 0;
+
 
 function init()
 {
@@ -20,11 +23,46 @@ function kepKirakas()
     
     let asztal = document.getElementById("asztal");
 
-    for(let i=0; i < kepekUrl.length;i++)
+    let kartyak = [];
+
+    for(let k = 0; k < 2; k++)
     {
-        let uj = document.createElement("img");
-        uj.src="kepek/" + kepekUrl[i];
-        asztal.appendChild(uj);
+        for(let i=0; i < parDb;i++)
+        {
+            /*let uj = document.createElement("img");
+            uj.src="kepek/" + kepekUrl[i];
+            asztal.appendChild(uj);
+            */
+
+            let uj = document.createElement("div");
+            uj.className="kartya";
+            uj.onclick=function()
+            {
+                uj.style.backgroundImage="url(kepek/" + kepekUrl[i] + ")"
+                kattintas++;
+            };
+            //asztal.appendChild(uj);
+            kartyak.push(uj);
+
+        }
+    }
+    kartyak=kever(kartyak);
+
+    for(let i=0; i < kartyak.length; i++)
+    {
+    asztal.appendChild(kartyak[i]);
+    }
+    
+}
+
+function kever(points) {
+
+    for (let i = points.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i+1));
+      let k = points[i];
+      points[i] = points[j];
+      points[j] = k;
     }
 
+    return points;
 }
